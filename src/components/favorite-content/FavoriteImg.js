b073e85ba39807cf9cc7c25391733c7b0d947130
igor mfe <igormafelipe@gmail.com>
2023-06-11 19:09:07 -0400

    Added see more button and github icon link. Adde animation to buttons, and made them central in projects card

--- src/components/favorite-content/FavoriteImg.js
+++ src/components/favorite-content/FavoriteImg.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { show } from './FavoriteDisplaySlicer';
+import GitHub from '../../images/favorite_page/github.png';
 
 function FavoriteImg(props) { 
   const outerDivStyle = {
@@ -29,17 +30,26 @@ function FavoriteImg(props) {
       {/* Information DIV */}
       {
         isHover &&
-        <div className="w-full h-full cursor-pointer"
-            onClick={() => dispatch(show(props))}
+        <div className="w-full h-full"
             style={outerDivStyle}>
             <div className="w-full h-full hover:h-full bg-black bg-opacity-70 transition-all">
               <h1 className="text-2xl text-white text-center relative top-5">{props.title}</h1>
               {/*Overview of project*/}
               <h1 className="text-2xl text-white text-center relative top-20">{props.body}</h1>
               {/*Skills used*/}
-              {/* <h1 className="text-2xl text-white text-center relative top-20">{props.body}</h1> */}
-              {/*Github and button to see more*/}
-              {/* <h1 className="text-2xl text-white text-center relative top-20">{props.body}</h1> */}
+              <h1 className="text-2xl text-white text-center relative top-40">Skills: Python, React, CSS</h1>
+              {/*Todo: send link as props!*/}
+              <div className="w-full relative top-60 flex justify-center space-x-10">
+                <a href="https://google.com" target="_blank">
+                  <img src={GitHub} 
+                        className="rounded-full w-12 h-12 hover:cursor-pointer animate-pulse hover:animate-none"/>
+                </a>
+
+                <button class="bg-black text-white border border-white hover:bg-yellow-500 hover:border-yellow-500 hover:text-black px-4 py-2 rounded animate-pulse hover:animate-none"
+                        onClick={() => dispatch(show(props))}>
+                  See More
+                </button>
+              </div>
             </div>
         </div>
       }
