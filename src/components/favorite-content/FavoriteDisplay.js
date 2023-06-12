68a72373c6c23a9b177fdd8dbfbbf9ed5245c326
igor mfe <igormafelipe@gmail.com>
2023-06-12 15:05:22 -0400

    template for projects see more

--- src/components/favorite-content/FavoriteDisplay.js
+++ src/components/favorite-content/FavoriteDisplay.js
@@ -15,11 +15,8 @@ function FavoriteDisplay(props) {
 
   return (
     <div className={showing ? showingClass : hiddenClass}>
-      <div className="relative top-0 w-5/6 h-5/6 flex flex-row">
-        <div className="w-1/2 h-full relative top-0 left-0">
-          <img className="absolute top-0 left-0 w-full h-full" src={imageSrc}/>
-        </div>
-        <div className="w-3/4 h-full relative left-0 top-0 bg-gray-200 overflow-auto">
+      <div className="relative top-0 w-full h-full flex flex-row">
+        <div className="w-full h-full relative left-0 top-0 bg-gray-200 overflow-auto">
           <div className="h-auto w-full relative top-0 left-0">
             <h1 className="font-extrabold text-3xl text-gray-800 pt-1 text-center font-mono">
               {title}
@@ -28,6 +25,19 @@ function FavoriteDisplay(props) {
           <div className="h-auto w-full relative top-10 left-0 overflow-hidden">
             <h1 className="font-extrabold text-2xl text-gray-800 pt-1 text-left font-mono px-3">
               {bodyText}
+            </h1>
+          </div>
+          <div className="w-full h-1/3 bg-red-300 relative top-20">
+            <h1>GALLERY</h1>
+          </div>
+          <div className="h-auto w-full relative top-10 left-0 overflow-hidden bg-red-500 relative top-40">
+            <h1 className="font-extrabold text-2xl text-gray-800 pt-1 text-left font-mono px-3">
+              Skills
+            </h1>
+          </div>
+          <div className="h-auto w-full relative top-10 left-0 overflow-hidden bg-red-500 relative top-60">
+            <h1 className="font-extrabold text-2xl text-gray-800 pt-1 text-left font-mono px-3">
+              Video
             </h1>
           </div>
         </div>
